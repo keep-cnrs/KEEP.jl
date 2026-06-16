70048e586128c971bae96c269acbee459e2aa410
abavoil <17646791+abavoil@users.noreply.github.com>
2026-06-16 17:53:26 +0200

    update env

--- test/00_test_aqua.jl
+++ test/00_test_aqua.jl
@@ -1,13 +1,20 @@
+using Aqua
+using Test
+using KEEP
+
 function test_aqua()
     @testset "Aqua.jl" begin
         Aqua.test_all(
             KEEP;
             ambiguities=false,
             #stale_deps=(ignore=[:SomePackage],),
-            deps_compat=(ignore=[:LinearAlgebra, :Unicode],),
+            deps_compat=(ignore=[:LinearAlgebra, :Logging],),
             piracies=true,
         )
         # do not warn about ambiguities in dependencies
         Aqua.test_ambiguities(KEEP)
     end
 end
+
+# Uncomment to get an error
+# test_aqua()
